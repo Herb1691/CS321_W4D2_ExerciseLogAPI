a431cf88ed881f9a660e5adb65c5e5ac96beddbc
Herb1691 <hwright_1691@hotmail.com>
2020-02-12 18:26:18 -0600

    Finally fixed missing table issue

--- CS321_W4D2_ExerciseLogAPI.Infrastructure/Data/AppDbContext.cs
+++ CS321_W4D2_ExerciseLogAPI.Infrastructure/Data/AppDbContext.cs
@@ -17,7 +17,7 @@ namespace CS321_W4D2_ExerciseLogAPI.Infrastructure.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // TODO: use optionsBuilder to configure a Sqlite db
-            optionsBuilder.UseSqlite("Data Source=ExerciseLog.db");
+            optionsBuilder.UseSqlite("Data Source=../CS321_W4D2_ExerciseLogAPI.Infrastructure/ExerciseLog.db");
         }
 
         // This method runs once when the DbContext is first used.
